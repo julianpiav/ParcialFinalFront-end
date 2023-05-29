fbbcde5e01c16b72e66a87b5f569c731ecda2ba3
julianpiav <julianpiav@unisabana.edu.co>
2023-05-29 11:32:31 -0500

    Correccion de asignaciones en los input de tarea

--- src/Componentes/FormularioPracticas.jsx
+++ src/Componentes/FormularioPracticas.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export const FormularioPracticas = ({agregar,dato,setDato,datoTarea,setDatoTarea, edicion,setEdicion,editar}) => {
-  const [inputList, setinputList]= useState([{nombreTarea:'', fechaTarea:''}]);
+  const [inputList, setinputList]= useState([{nombre:'', fecha:''}]);
 
   const enviar = (practica) => {
     if (edicion === true) {
@@ -23,8 +23,8 @@ export const FormularioPracticas = ({agregar,dato,setDato,datoTarea,setDatoTarea
   };
   const agregarInput=()=>{
     let inputNuevo={
-      nombreTarea:"",
-      fechaTarea:""
+      nombre:"",
+      fecha:""
     }
     setinputList([...inputList, inputNuevo]);
   }
@@ -104,7 +104,7 @@ export const FormularioPracticas = ({agregar,dato,setDato,datoTarea,setDatoTarea
                     placeholder=" "
                     value={datoTarea.nombreTarea}
                     onChange={(event) =>
-                      setDato({ ...datoTarea, nombreTarea: event.target.value })
+                      setDatoTarea({ ...datoTarea, nombreTarea: event.target.value })
                     }
                   />
                   <label htmlFor="nombreTarea" className="form-label">
@@ -119,7 +119,7 @@ export const FormularioPracticas = ({agregar,dato,setDato,datoTarea,setDatoTarea
                     placeholder=" "
                     value={datoTarea.fechaTarea}
                     onChange={(event) =>
-                      setDato({ ...datoTarea, fechaTarea: event.target.value })
+                      setDatoTarea({ ...datoTarea, fechaTarea: event.target.value })
                     }
                   />
                   <label htmlFor="nombreTarea" className="form-label">
